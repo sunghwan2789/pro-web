7ae62cc5cab4cadcbfe21660f40ced01974712c3
Sunghwan Bang <sunghwan2789@gmail.com>
2019-01-26 12:39:12 +0900

    POST /login 구현

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,36 +20,67 @@ namespace pro_web.Pages
         [FromQuery]
         public bool Registered { get; set; }
 
-        [FromQuery, FromBody]
+        [FromQuery]
         public string ReturnUrl { get; set; }
 
-        [FromBody]
-        public string StudentNumber { get; set; }
+        [FromForm]
+        [DataType(DataType.Text)]
+        public uint? StudentNumber { get; set; }
 
-        [FromBody]
+        [FromForm]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [FromBody]
+        [FromForm]
         public bool Persist { get; set; }
 
         public void OnGet()
         {
+            if (Registered)
+            {
+                ViewData["Message"] = @"
+                    <p>계정 설정을 완료했습니다.
+                    <p>다시 로그인하세요.";
+            }
         }
 
-        public async void OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
-                Response.StatusCode = 400;
-                return;
+                return Page();
             }
 
             var member = await db.Members.FindAsync(StudentNumber);
             if (member == null)
             {
-                Response.StatusCode = 401;
-                return;
+                ViewData["Message"] = @"
+                    <p>아이디가 없습니다.
+                    <p>회원이라면 관리자에게 문의하세요.";
+                return Page();
             }
+
+            if (member.Password == null)
+            {
+                return RedirectToPage("./Register", new
+                {
+                    ReturnUrl,
+                    StudentNumber,
+                });
+            }
+
+            // TODO: 비밀번호 검사
+
+            await db.MemberLogs.AddAsync(new Models.MemberLog
+            {
+                Member = member,
+                Text = "LOGIN",
+            });
+            await db.SaveChangesAsync();
+
+            // TODO: 세션 만들기
+
+            return Redirect(ReturnUrl ?? $"{Request.PathBase}/");
         }
     }
 }
