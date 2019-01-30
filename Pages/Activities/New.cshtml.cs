da8b6c1ff359c56498e336d5de2389064fc2b626
Sunghwan Bang <sunghwan2789@gmail.com>
2019-01-30 23:00:57 +0900

    POST /activities/new 구현

--- Pages/Activities/New.cshtml.cs
+++ Pages/Activities/New.cshtml.cs
@@ -1,24 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using pro_web.Models;
 
 namespace pro_web.Pages.Activities
 {
     public class NewModel : PageModel
     {
-        public NewModel(ProContext db)
+        public NewModel(ProContext db, IHostingEnvironment env)
         {
             this.db = db;
+            this.env = env;
         }
 
         private readonly ProContext db;
+        private readonly IHostingEnvironment env;
 
-        public Models.Activity Activity { get; set; }
+        [BindProperty]
+        public Activity Activity { get; set; }
 
-        public IEnumerable<Models.Member> Members { get; set; }
+        public IEnumerable<Member> Members { get; set; }
+
+        public IFormFileCollection Attachments { get; set; }
 
         public void OnGet()
         {
@@ -36,6 +45,49 @@ namespace pro_web.Pages.Activities
                 .ThenByDescending(i => i.Gen)
                 .ThenBy(i => i.StudentNumber)
                 .ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // TODO: 권한 확인
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            foreach (var attachment in Attachments)
+            {
+                var filename = $"{Guid.NewGuid()}{Path.GetExtension(attachment.FileName)}";
+                var path = Path.Combine(env.ContentRootPath, "storage", "attaches", filename);
+                using (var fs = new FileStream(path, FileMode.CreateNew))
+                {
+                    await attachment.CopyToAsync(fs);
+                }
+                Activity.Attachments.Add(new ActivityAttachment
+                {
+                    Filename = filename,
+                    MediaType = attachment.ContentType,
+                    OriginalFilename = attachment.FileName,
+                    Size = attachment.Length,
+                });
+            }
+
+            foreach (var attendeeStudentNumber in Request.Form["attend[]"].Select(uint.Parse))
+            {
+                Activity.ActivityAttendees.Add(new ActivityAttendee
+                {
+                    AttandeeId = attendeeStudentNumber,
+                });
+            }
+
+            await db.Activities.AddAsync(Activity);
+            await db.SaveChangesAsync();
+
+            return RedirectToPage("./Index", new
+            {
+                ActivityId = Activity.Id,
+            });
         }
     }
 }
