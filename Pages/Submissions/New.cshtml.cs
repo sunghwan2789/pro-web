5611d69be3490a00b45eade33d4580f172171f5d
Sunghwan Bang <sunghwan2789@gmail.com>
2019-02-02 18:30:56 +0900

    POST /submissions/new/{TaskId} 구현 (aka. /sources/new/{TaskId}

--- Pages/Submissions/New.cshtml.cs
+++ Pages/Submissions/New.cshtml.cs
@@ -1,20 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using pro_web.Services;
 
 namespace pro_web.Pages.Submissions
 {
     public class NewModel : PageModel
     {
-        public NewModel(ProContext db)
+        public NewModel(ProContext db, IHostingEnvironment env, ICompileAndGoQueue queue)
         {
             this.db = db;
+            this.env = env;
+            this.queue = queue;
         }
 
         private readonly ProContext db;
+        private readonly IHostingEnvironment env;
+        private readonly ICompileAndGoQueue queue;
 
         [FromRoute]
         public uint TaskId { get; set; }
@@ -30,6 +38,51 @@ namespace pro_web.Pages.Submissions
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync([FromForm] string source)
+        {
+            Task = await db.Tasks.FindAsync(TaskId);
+            if (Task == null)
+            {
+                return NotFound();
+            }
+
+            // TODO: ¼¼¼Ç
+            var authorId = 20154030u;
+
+            var path = Path.Combine(env.ContentRootPath, "storage", "sources", $"{Task.Id}_{authorId}_");
+            var i = 1;
+            while (System.IO.File.Exists(path + i + ".c"))
+            {
+                i++;
+            }
+            path += i + ".c";
+
+            var submission = new Models.TaskSource
+            {
+                Task = Task,
+                AuthorId = authorId,
+                Sequence = (uint)i,
+                Working = true,
+                Error = "",
+            };
+
+            using (var fs = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+            using (var sw = new StreamWriter(fs, new UTF8Encoding(false)))
+            {
+                await sw.WriteAsync(source);
+            }
+            submission.Size = (uint)new FileInfo(path).Length;
+
+            db.TaskSources.Add(submission);
+            await db.SaveChangesAsync();
+
+            queue.QueueBackgroundWorkItem(submission);
+            return RedirectToPage("/Submissions/Index", new
+            {
+                TaskId,
+            });
         }
     }
 }
