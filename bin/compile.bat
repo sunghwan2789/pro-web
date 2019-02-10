066b75455285283fc3a79cb6ff14bce5cccb3fc5
Sunghwan Bang <sunghwan2789@gmail.com>
2019-02-10 19:15:54 +0900

    레거시 코드 삭제

--- bin/compile.bat
+++
@@ -1,2 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-cl %1 /nologo /O2 /TC /Za /Fe%2 /Fo%3 >%4 2>&1
--- config/storage.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'attaches' => dirname(__DIR__) . '/storage/attaches',
-    'sources' => dirname(__DIR__) . '/storage/sources',
-];
