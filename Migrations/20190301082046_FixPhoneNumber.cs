04c6c1dd2543b9469ea3a2339ea13e4b77dae134
Sunghwan Bang <sunghwan2789@gmail.com>
2019-03-01 17:28:34 +0900

    전화번호 업데이트

---
+++ Migrations/20190301082046_FixPhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pro_web.Migrations
+{
+    public partial class FixPhoneNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Members SET PhoneNumber=NULL WHERE PhoneNumber='0'");
+            migrationBuilder.Sql("UPDATE Members SET PhoneNumber=CONCAT('0', PhoneNumber) WHERE PhoneNumber IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Members SET PhoneNumber='0' WHERE PhoneNumber IS NULL");
+        }
+    }
+}
